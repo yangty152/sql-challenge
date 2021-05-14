7a0a5d512fdae9f09093b1de4e2ceb2f39d02272
yangty152 <35477773+yangty152@users.noreply.github.com>
2021-05-13 22:05:06 -0400

    Orgnize the folders

--- EmployeeSQL/SQLScripts/selectscript.sql
+++ EmployeeSQL/SQLScripts/selectscript.sql
@@ -5,7 +5,7 @@ JOIN salaries s
 ON e.emp_no = s.emp_no;
 
 -- List first name, last name, and hire date for employees who were hired in 1986.
-SELECT first_name, last_name, hire_date 
+SELECT first_name AS "First Name", last_name AS "Last Name", hire_date AS "Hired Date"
 FROM employee
 WHERE (hire_date >= '1986-01-01') AND (hire_date<='1986-12-31');
 
@@ -20,7 +20,7 @@ ON e.emp_no = dm.emp_no;
 
 -- List the department of each employee with the following information: employee number, last name, first name, and department name.
 
-SELECT d.dept_name AS "Department Name", e.emp_no AS "Employee Number", e.last_name AS "Last Name", e.first_name AS "First Name"
+SELECT e.emp_no AS "Employee Number", e.last_name AS "Last Name", e.first_name AS "First Name",d.dept_name AS "Department Name"
 FROM employee e
 JOIN dept_emp de
 ON de.emp_no = e.emp_no
@@ -36,7 +36,7 @@ WHERE (first_name = 'Hercules') AND (last_name LIKE 'B%');
 
 -- List all employees in the Sales department, including their employee number, last name, first name, and department name.
 
-SELECT d.dept_name AS "Department Name", e.emp_no AS "Employee Number", e.last_name AS "Last Name", e.first_name AS "First Name"
+SELECT e.emp_no AS "Employee Number", e.last_name AS "Last Name", e.first_name AS "First Name",d.dept_name AS "Department Name"
 FROM employee e
 JOIN dept_emp de
 ON de.emp_no = e.emp_no
@@ -45,7 +45,7 @@ ON d.dept_no = de.dept_no
 WHERE d.dept_name = 'Sales';
 
 -- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
-SELECT d.dept_name AS "Department Name", e.emp_no AS "Employee Number", e.last_name AS "Last Name", e.first_name AS "First Name"
+SELECT e.emp_no AS "Employee Number", e.last_name AS "Last Name", e.first_name AS "First Name",d.dept_name AS "Department Name"
 FROM employee e
 JOIN dept_emp de
 ON de.emp_no = e.emp_no
